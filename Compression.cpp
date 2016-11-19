2b00e32d2b5ab247b995dda627a64e4ca68be5cc
orevron <orevron@gmail.com>
2016-11-19 10:14:32 +0200

    Update Compression.cpp

--- Compression.cpp
+++ Compression.cpp
@@ -47,20 +47,21 @@ bool Compression::UD_check() {
  * Looks for all danglings in the list of words, in order to confirm UD
  */
 bool Compression::isDanglingInCode() {
-    for (size_t i = 0; i < sizeof(dangling) / sizeof(int); i++) {
-        if (contains(dangling[i])) {
+    for(size_t i = 0; i < sizeof(dangling) / sizeof(int); i++) {
+        if(contains(dangling[i])){
             return false;
         }
-        return true;
     }
-}
+    return true;
 
+}
 /**
  * Looks for one dangling in the list of words
  */
 bool Compression::contains(int word) {
-    for (size_t i = 0; i < sizeof(list_of_words) / sizeof(int); i++) {
-        if (word == i) {
+    int size = sizeof(list_of_words) / sizeof(int);
+    for(int i = 0; i < size; i++){
+        if(word == i){
             return true;
         }
     }
